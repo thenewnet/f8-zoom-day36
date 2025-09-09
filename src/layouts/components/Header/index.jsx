806caa4b0120861d8af9f81b543c2ece31f356b9
trungbq <trungbq86@gmail.com>
2025-09-09 17:18:24 +0700

    add header common

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -1,12 +1,50 @@
+import { Home, Info, Phone, FileText, Shield, Settings, Users } from "lucide-react";
+import clsx from "clsx";
+import styles from "./Header.module.scss";
+import { NavLink } from "react-router-dom";
 
-import styles from './Header.module.scss';
-
+const navItems = [
+    { to: '/', title: 'Home', icon: <Home size={18} /> },
+    { to: '/about', title: 'About', icon: <Info size={18} /> },
+    { to: '/contact', title: 'Contact', icon: <Phone size={18} /> },
+    { to: '/posts', title: 'Posts', icon: <FileText size={18} /> },
+    { to: '/privacy', title: 'Privacy', icon: <Shield size={18} /> },
+    { to: '/admin/settings', title: 'Settings', icon: <Settings size={18} /> },
+    { to: '/admin/user', title: 'Users', icon: <Users size={18} /> },
+];
 function Header() {
     return (
-        <div className="header">
-            <h1>Header</h1>
-        </div>
-    )
+        <header className={styles.siteHeader}>
+            <div className={clsx(styles.container, styles.wrapper)}>
+                {/* Logo */}
+                <div className={styles.logo}>
+                    <img src="./logo.svg" alt="Logo" />
+                </div>
+
+                {/* Navigation */}
+                <nav className={styles.nav}>
+                    <ul>
+                        {navItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <NavLink to={item.to}
+                                        className={({ isActive }) => clsx(styles.navItem, isActive && styles.active)}>
+                                        {item.icon} {item.title}
+                                    </NavLink>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+
+                {/* Actions */}
+                <div className={styles.actions}>
+                    <button className={styles.btn}>Login</button>
+                    <button className={clsx(styles.btn, styles.btnPrimary)}>Register</button>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
